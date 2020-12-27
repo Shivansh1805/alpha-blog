a95f7c05854cb68d0d96f29df24e86ad96564fff
Shivansh1805 <iec2017054@iiita.ac.in>
2020-12-27 13:28:43 +0530

    Added editing of the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,21 @@ class ArticlesController < ApplicationController
     def show 
       @article = Article.find(params[:id])
     end
-    
+
+    def edit
+      @article = Article.find(params[:id])  
+    end
+
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(article_param)
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+          else
+            render 'edit'
+          end
+    end
+
     def create  
       @article = Article.new(article_param)
       if @article.save
