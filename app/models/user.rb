b071136479ccba4d26e8844e564aa819f8c50088
Shivansh1805 <iec2017054@iiita.ac.in>
2020-12-30 18:57:50 +0530

    Added signup and password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
                      uniqueness: {case_sensitive: false},
                      length: {minimum: 5,maximum:125},
                      format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
